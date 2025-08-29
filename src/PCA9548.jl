288d45ebf7463f9a9cd68b96e664cc389e35f8c7
Nick Stone <nicholasestone@proton.me>
2025-08-29 13:59:18 -0500

    initial commit

---
+++ src/PCA9548.jl
@@ -0,0 +1,26 @@
+module PCA9548
+
+using PyCall
+
+export MultiI2C
+
+"""
+```julia
+MultiI2C(i2c)
+```
+Build an object to control an i2c multiplexer attached via the provided i2c bus.
+Channels on this multiplexer can be accessed using `m[channel]` indexing syntax.
+"""
+struct MultiI2C
+    multi
+    function MultiI2C(i2c)
+        lib = pyimport("adafruit_tca9548a")
+        new(lib.TCA9548A(i2c))
+    end
+end
+
+Base.getindex(m::MultiI2C,i::Int) = m.multi[i]
+Base.firstindex(::MultiI2C) = 0
+Base.lastindex(::MultiI2C) = 7
+
+end # module PCA9548
